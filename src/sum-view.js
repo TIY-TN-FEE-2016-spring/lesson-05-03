f5bfcdc264a9b7e46d19b3c6ae4920aae1c38670
James Dabbs <jamesdabbs@gmail.com>
2016-03-30 10:58:07 -0500

    Fetch sum from server

--- src/sum-view.js
+++ src/sum-view.js
@@ -1,11 +1,19 @@
 export default class SumView {
   constructor(el) {
     this.el = el
+    this.result = 'Loading ...'
 
     this.redraw()
+
+    fetch(`http://localhost:3000/users/james/numbers/sum`)
+      .then(r => r.json())
+      .then(sum => {
+        this.result = sum
+        this.redraw()
+      })
   }
 
   redraw() {
-    this.el.innerHTML = `<p>Sum is:</p><h2>7</h2>`
+    this.el.innerHTML = `<p>Sum is:</p><h2>${this.result}</h2>`
   }
 }
