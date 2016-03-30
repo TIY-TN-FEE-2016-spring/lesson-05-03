7690d43abf6a0335af403e71b83ebc177a11e456
James Dabbs <jamesdabbs@gmail.com>
2016-03-30 12:04:10 -0500

    Bind delete button; sketch actual call

--- src/number-list-view.js
+++ src/number-list-view.js
@@ -18,9 +18,24 @@ export default class NumberListView {
 
     this.numbers.forEach(i => {
       // content = content + `<li>${i}</li>`
-      content += `<li>${i}</li>`
+      content += `<li>
+        ${i}
+        <button>X</button>
+      </li>`
     })
 
     this.pageSection.innerHTML = content
+
+    const buttons = this.pageSection.querySelectorAll(`button`)
+    for (let i=0; i < buttons.length; i++) {
+      let button = buttons[i];
+      button.addEventListener(`click`, () => {
+        this.removeNumberAtIndex(i)
+      })
+    }
+  }
+
+  removeNumberAtIndex(index) {
+    console.log('should remove number at index', index)
   }
 }
